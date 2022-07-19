a9c46400e6dc8fb0fd954f9a45ee3bd1708f433d
surtarso <tarso.galvao@gmail.com>
2022-07-19 20:29:11 -0300

    reduced clutter

--- My Landing Page/src/rocket.js
+++ My Landing Page/src/rocket.js
@@ -1,10 +1,11 @@
 var count = 0;
 var rocket = document.getElementById('rocket');
 
-// activate hidden game link
+// activate hidden game link on mouse up
 rocket.addEventListener('mouseup', function(evt){
     count++;
     if(count == 7){
+        //insert link to outter html (creates a new element/object)
         rocket.outerHTML = '<a id="rocket" href="https://tarsogalvao.ddns.net/games/bullethell"><img src="./img/rocket2.webp" id="Rocket"></a>';
         setTimeout(function(){
             //get new reference of element (to the changed one above)
