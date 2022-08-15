a2846ccf41ee644c6fbab8c1f540fcc22b188595
surtarso <tarso.galvao@gmail.com>
2022-08-15 15:12:41 -0300

    added canvas

--- My Landing Page/src/js/canvas.js
+++ My Landing Page/src/js/canvas.js
@@ -10,8 +10,8 @@ window.addEventListener('resize', function(){
     CANVAS.height = window.innerHeight;
 });
 
-var ball_size = 15;
-var ball_speed = 20;
+var ball_size = 30;
+var ball_speed = 1;
 var ball_loc_x, ball_loc_y;  // ball location
 var ball_dir_x, ball_dir_y; // ball direction
 
@@ -22,14 +22,6 @@ function init(){
     ball_dir_y = -ball_speed;
 }
 
-//draws rectangles (brick/paddle/bg)
-function drawRect(color, x, y, w, h) {
-    CONTEXT.fillStyle = color
-    CONTEXT.beginPath()
-    CONTEXT.rect(x, y, w, h)
-    CONTEXT.fill()
-    CONTEXT.stroke()
-}
 //draw circles (ball)
 function drawCircle(color, x, y, r) {
     CONTEXT.fillStyle = color
@@ -37,64 +29,40 @@ function drawCircle(color, x, y, r) {
     CONTEXT.arc(x, y, r, 0, 2 * Math.PI, false)
     CONTEXT.fill()
 }
+
 //bounce on canvas walls
 function bounce(){
-    //bounce on walls
     if (ball_loc_x - ball_size + ball_dir_x < 0 ||
             ball_loc_x + ball_size + ball_dir_x > CANVAS.width){
         ball_dir_x = -ball_dir_x;
     };
     if (ball_loc_y - ball_size + ball_dir_y < 0 ||
-        ball_loc_y + ball_size + ball_dir_y > CANVAS.height){
+            ball_loc_y + ball_size + ball_dir_y > CANVAS.height){
         ball_dir_y = -ball_dir_y;
     };
 }
+
 //draw game details
-function draw(){
-    CONTEXT.globalAlpha = 0;
-    //background
-    drawRect('black', 0, 0, CANVAS.width, CANVAS.height);
-    CONTEXT.globalAlpha = 1;
-    //blackhole
-    // drawCircle('lightgray', ball_loc_x, ball_loc_y, ball_size);
-    // drawCircle('#1a1b1a', (ball_loc_x-1), (ball_loc_y-1), (ball_size-1));
-    drawCircle('black', (ball_loc_x-2), (ball_loc_y-2), (ball_size-6));
+function drawBall(){
+    clearCanvas();
+    drawCircle('whitesmoke', (ball_loc_x-2), (ball_loc_y-2), (ball_size-6));
+    drawCircle('gray', (ball_loc_x-2), (ball_loc_y-2), (ball_size-7));
+    drawCircle('black', (ball_loc_x-2), (ball_loc_y-2), (ball_size-10));
+}
+
+//clear canvas
+function clearCanvas(){
+    CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
 }
 
 //main update on interval
 function updateGame(){
-    draw();
+    drawBall();
     bounce();
     //ball movement
-    // ball_loc_x += ball_dir_x;
-    // ball_loc_y += ball_dir_y;
+    ball_loc_x += ball_dir_x;
+    ball_loc_y += ball_dir_y;
 }
 
-//movement
-// document.addEventListener("keydown", function(e){
-//     switch (e.keyCode) {
-//         case 37: //left
-//             if (ball_loc_x >= (ball_size/2)){
-//                 ball_loc_x -= ball_dir_x;
-//                 break;
-//             }
-//         case 38: //up
-//             if (ball_loc_y <= CANVAS.height - (ball_size/2)){
-//                 ball_loc_y += ball_dir_y;
-//                 break;
-//             }
-//         case 39: //right
-//             if (ball_loc_x <= CANVAS.width - (ball_size/2)){
-//                 ball_loc_x += ball_dir_x;
-//                 break;
-//             }
-//         case 40: //down
-//             if (ball_loc_y >= (ball_size/2)){
-//                 ball_loc_y -= ball_dir_y;
-//                 break;
-//             }
-//     };
-// });
-
 init();
-setInterval(() => {updateGame()}, 1000/30);
+setInterval(() => {updateGame()}, 1000/60);
